7229f03e1cc109fadd8392e389a34d3064d618ba
kyunakim <kyunacloud@gmail.com>
2022-07-25 10:21:32 +0900

    kyuna

--- Bbompage_yellow/script.js
+++ Bbompage_yellow/script.js
@@ -1,23 +1,22 @@
-//selecting all required elements
+//전체 선택
 const filterItem = document.querySelector(".items");
 const filterImg = document.querySelectorAll(".gallery .image");
 
-window.onload = ()=>{ //after window loaded
-  filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
-    if(selectedItem.target.classList.contains("item")){ //if user selected item has .item class
-      filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
-      selectedItem.target.classList.add("active"); //add that active class on user selected item
-      let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+window.onload = ()=>{ 
+  filterItem.onclick = (selectedItem)=>{ //필터링
+    if(selectedItem.target.classList.contains("item")){ // nav item 클릭했을 때
+      filterItem.querySelector(".active").classList.remove("active"); // remove
+      selectedItem.target.classList.add("active"); //add
+      let filterName = selectedItem.target.getAttribute("data-name"); 
       filterImg.forEach((image) => {
-        let filterImges = image.getAttribute("data-name"); //getting image data-name value
-        //if user selected item data-name value is equal to images data-name value
-        //or user selected item data-name value is equal to "all"
+        let filterImges = image.getAttribute("data-name"); // value별 불러오기
+
         if((filterImges == filterName) || (filterName == "all")){
-          image.classList.remove("hide"); //first remove the hide class from the image
-          image.classList.add("show"); //add show class in image
+          image.classList.remove("hide"); 
+          image.classList.add("show"); 
         }else{
-          image.classList.add("hide"); //add hide class in image
-          image.classList.remove("show"); //remove show class from the image
+          image.classList.add("hide");
+          image.classList.remove("show"); 
         }
       });
     }
